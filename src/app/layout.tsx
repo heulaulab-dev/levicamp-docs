d24c53c70e187b5ad8a50bbb21884a1d4fa96f36
fullvomitforme <tazkiyadigitalarchive@gmail.com>
2025-06-02 17:26:12 +0700

    Update documentation for LeviCamp platform, including new content structure and features. Remove unused home layout and page files, and adjust layout configurations for direct access to documentation. Enhance navigation with specific guides for admin and customer functionalities.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { baseOptions } from '@/app/layout.config';
+import { source } from '@/lib/source';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -9,10 +12,14 @@ const inter = Inter({
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
-  );
+		<html lang='en' className={inter.className} suppressHydrationWarning>
+			<body className='flex flex-col min-h-screen'>
+				<RootProvider>
+					<DocsLayout tree={source.pageTree} {...baseOptions}>
+						{children}
+					</DocsLayout>
+				</RootProvider>
+			</body>
+		</html>
+	);
 }
